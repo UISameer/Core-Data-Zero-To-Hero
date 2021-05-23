1d70e6974ca6681ded4fe7cda97cece5783a53ab
Sameer Jagtap <jagtap.sameer00@gmail.com>
2021-05-24 00:40:39 +0530

    Added Github project with Fetch, Save, Update and Unique Constraint.

---
+++ Github Commits/Commit+CoreDataClass.swift
@@ -0,0 +1,7 @@
+import Foundation
+import CoreData
+
+@objc(Commit)
+public class Commit: NSManagedObject {
+
+}
---
+++ Github Commits/Commit+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+import Foundation
+import CoreData
+
+
+extension Commit {
+
+    @nonobjc public class func createFetchRequest() -> NSFetchRequest<Commit> {
+        return NSFetchRequest<Commit>(entityName: "Commit")
+    }
+
+    @NSManaged public var url: String
+    @NSManaged public var message: String
+    @NSManaged public var date: Date
+    @NSManaged public var sha: String
+
+}
+
+extension Commit : Identifiable {
+
+}
